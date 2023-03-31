80c65e9b9d37a12b911e9776c93bcb03a6e62ab6
dillk <guidill088@gmail.com>
2023-03-31 11:34:41 -0300

    pt2..

--- script/script.js
+++ script/script.js
@@ -1,6 +1,7 @@
 let inputBuscarFilme = document.querySelector("#input-buscar-filme");
 let btnBuscarFilme = document.querySelector("#btn-buscar-filme");
 
+
 btnBuscarFilme.onclick = () => {
     if(inputBuscarFilme.value.length > 0){
         fetch(""+inputBuscarFilme.value, {mode:"cors"})
@@ -15,11 +16,41 @@ btnBuscarFilme.onclick = () => {
 btnBuscarFilme.onclick = () => {
     if(inputBuscarFilme.value.lenght > 0)
     {
-        fetch("http://www.omdbapi.com/?i=tt3896198&apikey=7520e0a0&s=" +inputBuscarFilme.value, {mode:"cors"})
+        let filmes = new Array();
+        fetch("http://www.omdbapi.com/?i=tt3896198&apikey=7520e0a0&s=" +inputBuscarFilme.value)
         .then((resp) => resp.json())
-        .then((restp) => {
-            console.log(resp);
+        .then((resp) => {
+            resp.Search.forEach((item) => {
+                console.log(item);
+                let filme = new Filme(
+                    item.imdbID,
+                    item.Title,
+                    item.Year,
+                    null,
+                    null,
+                    item.Poster,
+                    null,
+                    null,
+                    null,
+                    null,
+                    null
+                );
+                filmes.push(filme);
+            });
+            listarFilmes(filmes);
         })
     }
     return false;
+}
+
+let listarFilmes = async (filmes) => {
+    let listaFilmes = await document.querySelector("#lista-filmes");
+    listaFilmes.innerHTML = "";
+    console.log(listaFilmes);
+    if(filme.lenght > 0)
+    {
+        filmes.forEach(async(filme) =>{
+            listaFilmes.appendChild(await filme.getCard());
+        })
+    }
 }
